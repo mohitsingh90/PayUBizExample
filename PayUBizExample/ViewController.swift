2ae65bf6b6cf1dbfc167acda382c1bc85b1609ce
Mohit <mohitsingh@outlook.in>
2018-01-27 19:53:29 +0530

    Initial Commit

---
+++ PayUBizExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PayUBizExample
+//
+//  Created by MOHIT SINGH on 27/01/18.
+//  Copyright © 2018 MOHIT SINGH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
